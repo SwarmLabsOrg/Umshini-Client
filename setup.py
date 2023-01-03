5aa836975dd5999aa8079553ea0d24b9b365c0b7
Ryan Sullivan <ryanpnavillus@gmail.com>
2023-01-03 01:36:14 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -1,18 +1,49 @@
 from setuptools import find_packages, setup
-import codecs
-import os
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from Cython.Build import cythonize
+import numpy as np
+import atexit
+from subprocess import check_call
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        print("pre develop")
+        develop.run(self)
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        check_call("AutoROM --accept-license".split())
+        print("post develop")
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        print("pre install")
+        install.run(self)
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        check_call("AutoROM --accept-license".split())
+        print("post install")
+
 
 VERSION = '0.0.1'
 DESCRIPTION = 'Umshini Client API'
+with open("README.md", 'r') as f:
+    long_description = f.read()
 
 setup(
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
     name='umshini',
     version=VERSION,
     author='umshini team',
     author_email="justinkterry@gmail.com",
     description=DESCRIPTION,
     url='umshini.ml',
-    #long_description=long_description,
+    long_description=long_description,
     #long_description_content_type="text/markdown",
     #keywords=["Reinforcement Learning", "game", "RL", "AI", "gym"],
     #python_requires=">=3.6, <3.10",
@@ -20,9 +51,21 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        'Cython',
         'colorama',
-        'pettingzoo[atari,classic]==1.22.2',
-        'supersuit==3.7.0',
+        'pettingzoo[atari,classic]==1.22.3',
+        'supersuit==3.7.1',
+        'autorom[accept-rom-license]',
+        'gymnasium',
+        'numpy',
+        'halo',
+    ],
+    setup_requires=[
+        'Cython',
+        'colorama',
+        'pettingzoo[atari,classic]==1.22.3',
+        'supersuit==3.7.1',
+        'autorom[accept-rom-license]',
         'gymnasium',
         'numpy',
         'halo',
