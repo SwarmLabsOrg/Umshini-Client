0a7b206cda7d14e888f536fc0e5dccc0ba8d0ecb
Andrew Tan <andrew.jinshen@gmail.com>
2023-09-10 23:18:37 +0800

    Game server error handling (#46)

--- umshini/tournament_client.py
+++ umshini/tournament_client.py
@@ -94,7 +94,14 @@ class NetworkEnv(gym.Env):
         # Receive observation from game server
         if self.verbose > 1:
             print("receiving obs")
-        observation_data = recv_json(self.game_connection)
+        try:
+            observation_data = recv_json(self.game_connection)
+        except Exception as e:
+            self.spinner.stop()
+            print(Fore.YELLOW + "Environment terminated prematurely! Round drawn.")
+            if self.verbose:
+                print(e)
+            return self.obs, 0, True, True, {}
         if self.verbose > 1:
             print("received obs")
         if observation_data["type"] != "observation":
@@ -287,7 +294,7 @@ class TournamentConnection:
         )
         spinner.start()
         try:
-            ready_data = recv_json(self.main_connection, timeout=180)
+            ready_data = recv_json(self.main_connection, timeout=600)
         except TimeoutError as err:
             print("Not enough players to start tournament.", flush=True)
             raise err
